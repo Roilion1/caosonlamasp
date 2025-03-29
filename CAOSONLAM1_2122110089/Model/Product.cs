e1bfe95c0e6991b25ac30ab1e76994ee5065258b
Roilion <caosonlam3003@gmail.com>
2025-03-29 08:59:59 +0700

    first commit

---
+++ CAOSONLAM1_2122110089/Model/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace CAOSONLAM1_2122110089.Model
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public float Price { get; set; }
+        public string Avata { get; set; }
+    }
+}
